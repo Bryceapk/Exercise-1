64a96cee086ad501782be9ee14cc307a2c38eace
Bryceapk <Bryceapk1@gmail.com>
2023-10-29 00:46:50 -0500

    Exercise1

---
+++ index.js
@@ -0,0 +1,10 @@
+let name="Bryce"
+let city="Atlanta"
+let state="Geogia"
+
+console.log("My name is", name)
+console.log("The city I'm from is", city)
+console.log("The state I'm from is", state)
+
+
+
